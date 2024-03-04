0a97f5d6b4e2f2045d4c9ced89ee617031cb32e2
JilGC <105502236+JilGC@users.noreply.github.com>
2024-03-04 15:08:26 -0400

    data.json in public folder and fetch data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
+import { useCallback, useEffect, useState } from 'react';
 import { BiCalendar } from "react-icons/bi";
 import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
 import Search from "./components/Search";
-import appointmentList from "./data.json";
+// import appointmentList from "./data.json";
 
 function App() {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptName.toLowerCase().includes(query.toLowerCase())
+
+      )
+    }
+  )
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
   return (
     <div className="App">
       <h1 className="text-5xl mb-3 m-3">
         <BiCalendar className="inline-flex text-red-400 align-top" />This is a reactApp</h1>
 
       <AddAppointment />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)} />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList
-          .map(appointment =>
-          (
+        {filteredAppointments
+          .map(appointment => (
             <AppointmentInfo key={appointment.id}
               appointment={appointment}
+              onDeleteAppointment={
+                appointmentId =>
+                  setAppointmentList(appointmentList.filter(appointment =>
+                    appointment.id === appointmentId))
+              }
             />
           ))
         }
       </ul>
 
-      <Search />
     </div>
   );
 }
